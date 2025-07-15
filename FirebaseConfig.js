82dee020c0c730a43608433c0469bc6827fb05d7
solanajuneil <131723715+solanajuneil@users.noreply.github.com>
2025-07-15 10:51:49 +0800

    Add files via upload

---
+++ FirebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCJKbwRCf6k092IDo-2x2IMRKTgJwO7msc",
+  authDomain: "auth-e19e1.firebaseapp.com",
+  projectId: "auth-e19e1",
+  storageBucket: "auth-e19e1.appspot.com",
+  messagingSenderId: "936296917510",
+  appId: "1:936296917510:web:0dfab5c6d87cfc6e20b187"
+};
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
